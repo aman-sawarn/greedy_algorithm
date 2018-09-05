9e7a983c30275b3ff64c447b8816f24281c16819
sawarn69 <sawarn69@gmail.com>
2018-09-06 01:09:39 +0530

    Final touches
    After running the code, you may get diffeent outputs at different run. It is because of the greedy algorithm, as it takes the local maxima or the minima, instead of the global maxima or minima.
    I shall soon upload a more efficient algorithm, which shall optimize the problem in a much better way.

--- Use_of_greedy_algorithm.py
+++ Use_of_greedy_algorithm.py
@@ -43,3 +43,18 @@ def testGreedy(items, constraint, keyFunction):
     print('Total value of items taken =', val)
     for item in taken:
         print(' ', item)
+
+def testGreedys(foods,maxUnits):
+    print('Use greedy by value to allocate', maxUnits,'calories')
+    testGreedy(foods, maxUnits, food.getValue)
+    print('\nUse greedy by cost to allocate', maxUnits,'calories')
+    testGreedy(foods, maxUnits, lambda x: 1/food.getCost(x))
+    print('\nUse greedy by density to allocate', maxUnits,'calories')
+    testGreedy(foods, maxUnits, food.density)
+
+
+names = ['Chicken', 'Banana Shake', 'Pizza', 'Burger', 'French Fries','Coke', 'Apple', 'Mango', 'Cake']
+values = [89,90,95,100,90,79,50,10]
+calories = [123,154,258,354,365,150,95,195]
+foods = buildMenu(names, values, calories)
+testGreedys(foods, 1000) 
