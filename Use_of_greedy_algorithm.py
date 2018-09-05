d610c89f9b07077ec3fa1f7d1816ed4e4aee4075
sawarn69 <sawarn69@gmail.com>
2018-09-06 01:04:11 +0530

    Code for the greedy function

--- Use_of_greedy_algorithm.py
+++ Use_of_greedy_algorithm.py
@@ -19,3 +19,21 @@ def buildMenu(names, values, calories):
         menu.append(food(names[i], values[i], calories[i]))
     return menu
     
+
+
+def greedy(items, maxCost, keyFunction):
+    
+    
+    """Assumes items a list, maxCost >= 0,
+       keyFunction maps elements of items to numbers"""
+    itemsCopy = sorted(items, key = keyFunction,
+    reverse = True)
+    result = []
+    totalValue, totalCost = 0.0, 0.0
+    for i in range(len(itemsCopy)):
+        if (totalCost + itemsCopy[i].getCost()) <= maxCost:
+            result.append(itemsCopy[i])
+            totalCost += itemsCopy[i].getCost()
+            totalValue += itemsCopy[i].getValue()
+    return (result, totalValue)     
+    
